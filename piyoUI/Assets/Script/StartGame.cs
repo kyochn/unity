ead9b90257af1472350d6c68df6054ea53e1fc0c
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-02-02 04:53:37 +0900

    StartButtonでシーンを遷移するようにした

---
+++ piyoUI/Assets/Script/StartGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //シーン遷移用のライブラリ
+
+public class StartGame : MonoBehaviour {
+    public void LoadingNewScene () {
+        //LoadSceneで任意のシーンを読み込む
+        SceneManager.LoadScene ("Main");
+    }
+}
