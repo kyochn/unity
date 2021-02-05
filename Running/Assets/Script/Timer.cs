3c5282166b0c5dffa37385ddc34ea346b9ec67bb
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-02-06 04:40:45 +0900

    タイマー機能を追加

---
+++ Running/Assets/Script/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public static float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Goal.goal==false){
+            time += Time.deltaTime; //timeに経過時間を足す
+            int t=Mathf.FloorToInt(time); //timeの小数点以下を切り捨てる
+            Text uiText=GetComponent<Text>(); //このスクリプトがアタッチされているオブジェクトのコンポーネントを取得
+            uiText.text="Time:" + t;  //テキストを変更
+        }
+    }
+}
