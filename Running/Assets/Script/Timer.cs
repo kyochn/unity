c4dc9f2b2e54e8a91a40b933cd36f72d5ef33ad7
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-02-06 05:02:17 +0900

    落ちてもタイマーがリセットされないようにした

--- Running/Assets/Script/Timer.cs
+++ Running/Assets/Script/Timer.cs
@@ -5,12 +5,7 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public static float time;
-    // Start is called before the first frame update
-    void Start()
-    {
-        time = 0;
-    }
+    public static float time = 0;
 
     // Update is called once per frame
     void Update()
