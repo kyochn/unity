dc4241aa3ccfa3c94f15dc2e1c1052ec46e372c4
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-02-07 04:58:24 +0900

    プラットフォームをAndroidに変更

---
+++ RunningMobile/Assets/Script/Out.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Out : MonoBehaviour
+{
+   void OnTriggerEnter(Collider other){ //判定エリアに接触した場合に自動的に呼び出される関数
+       if(other.gameObject.tag=="Player"){ //Playerタグが触れた時に処理を開始するようにする
+           SceneManager.LoadScene(SceneManager.GetActiveScene().name); //SceneManager.GetActiveScene().nameは現在のシーンの名前
+       }
+   }
+}
---
+++ RunningMobile/Assets/Script/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public static float time = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Goal.goal==false){
+            time += Time.deltaTime; //timeに経過時間を足す
+            int t=Mathf.FloorToInt(time); //timeの小数点以下を切り捨てる
+            Text uiText=GetComponent<Text>(); //このスクリプトがアタッチされているオブジェクトのコンポーネントを取得
+            uiText.text="Time:" + t;  //テキストを変更
+        }
+    }
+}
